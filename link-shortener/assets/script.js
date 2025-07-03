55e0d1d13c65e9699acb07ed878f6de62d1d7cfa
Vipnunes <otavio@vipnunes.com>
2025-07-02 22:12:42 -0300

    Update script.js

--- link-shortener/assets/script.js
+++ link-shortener/assets/script.js
@@ -22,7 +22,7 @@ document.getElementById("myinput").onclick = function () {
     // data to be sent to the API
     var data = {
         // domain -- change here if you want to use your own domain
-        domain: "i.vipnunes.com",
+        domain: "shortend.short.gy",
         originalURL: link,
         allowDuplicates: false,
     };
